e315b6bf81c8d53b9f2e14180a33f196620817e7
John Paul Hook <johnpaul82413@gmail.com>
2024-06-06 15:00:46 -0500

    shooting game

--- examples/shoot-the-fruit/shoot.py
+++ examples/shoot-the-fruit/shoot.py
@@ -1,10 +1,19 @@
-import pgzrun
-import pygame
-from pygame.base import *
+from random import randint
 apple = Actor("apple")
 
 def draw():
     screen.clear()
     apple.draw()
-
-
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+    
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("good shoot")
+        place_apple()
+    else:
+        print("you missed!")
+        quit()
+        
+place_apple()
